067bdf191b2ff51801f4192eb3879cd1ccfa4f3c
Abhay Pande <abhaypande@abhays-mbp.lan>
2020-06-10 01:29:13 -0700

    added changes for new test cases

--- rust/src/sqlparser/parser_tests.rs
+++ rust/src/sqlparser/parser_tests.rs
@@ -148,6 +148,56 @@ fn test_simple_where_lt_success(){
     }])
 }
 #[test]
+fn test_simple_where_ne_success(){
+    time_test!();
+    let p = parser::SQLParser::new();
+    let q = "select a, c, d FROM 'b' where a != '1'";
+    let (query, error_code)  = p.parse(q);
+    // println!("{}",query.table);
+    // println!("{:?}",query.fields);
+    assert!(error_code == ast::ErrorCode::None);
+    assert!(query.query_type ==  ast::QueryType::SELECT);
+    assert!(query.table == String::from( "b"));
+    assert!(query.fields == vec![String::from("a"), String::from("c"), String::from("d")]);
+    println!("{}",query.conditions[0].operand1);
+    assert!(query.conditions == vec![ast::Condition{
+        operand1: String::from("a"),
+        operator:  ast::Operator::ne,
+        operand2: String::from("1"),
+        operand_1_is_field : true,
+        operand_2_is_field : false
+    }])
+}
+#[test]
+fn test_simple_where_and_success(){
+    time_test!();
+    let p = parser::SQLParser::new();
+    let q = "select a, c, d FROM 'b' where a != '1' and b = '2'";
+    let (query, error_code)  = p.parse(q);
+    // println!("{}",query.table);
+    // println!("{:?}",query.fields);
+    assert!(error_code == ast::ErrorCode::None);
+    assert!(query.query_type ==  ast::QueryType::SELECT);
+    assert!(query.table == String::from( "b"));
+    assert!(query.fields == vec![String::from("a"), String::from("c"), String::from("d")]);  
+    assert!(query.conditions == vec![ast::Condition{
+        operand1: String::from("a"),
+        operator:  ast::Operator::ne,
+        operand2: String::from("1"),
+        operand_1_is_field : true,
+        operand_2_is_field : false
+    },
+    ast::Condition{
+        operand1: String::from("b"),
+        operator:  ast::Operator::eq,
+        operand2: String::from("2"),
+        operand_1_is_field : true,
+        operand_2_is_field : false
+    }
+
+    
+    ]) 
+}
 fn test_simple_where_star_success(){
     time_test!();
     let p = parser::SQLParser::new();
@@ -162,6 +212,29 @@ fn test_simple_where_star_success(){
 }
 #[test]
 fn test_update_success(){
+    time_test!();
+    let p = parser::SQLParser::new();
+    let q = "UPDATE 'a' SET b = 'hello' WHERE a = '1'";
+    let (query, error_code)  = p.parse(q);
+    // println!("{}",query.table);
+    // println!("{:?}",query.fields);
+    let mut  h = std::collections::HashMap::new();
+    h.insert(String::from("b"),String::from("hello"));
+    assert!(error_code == ast::ErrorCode::None);
+    assert!(query.query_type ==  ast::QueryType::UPDATE);
+    assert!(query.table == String::from( "a"));
+    assert_eq!(query.update_fields, h );   
+    assert!(query.conditions == vec![ast::Condition{
+        operand1: String::from("a"),
+        operator:  ast::Operator::eq,
+        operand2: String::from("1"),
+        operand_1_is_field : true,
+        operand_2_is_field : false
+    }
+    ]);
+}
+#[test]
+fn test_update_success_multiple(){
     time_test!();
     let p = parser::SQLParser::new();
     let q = "UPDATE 'a' SET b = 'hello', c = 'bye' WHERE a = '1' AND b = '789'";
